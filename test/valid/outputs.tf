ab94ed98ccf82048830426a53d86a323230b76cf
Billy Walker <billy@coresolutions.ltd>
2026-01-07 14:46:02 +0000

    chore: updating valid test config

--- test/valid/outputs.tf
+++ test/valid/outputs.tf
@@ -1,7 +1,9 @@
 output "environment" {
+  description = "Environment name for the test configuration."
   value = var.env
 }
 
 output "region" {
+  description = "Region for the test configuration."
   value = var.region
 }
